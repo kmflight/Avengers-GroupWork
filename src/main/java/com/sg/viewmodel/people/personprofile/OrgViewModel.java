74499935aeb5c4a0867dbd61d24027ec6a2f14f7
kmflight <kayla.flight@libertymutual.com>
2018-04-11 08:28:47 -0400

    Commiting updates to ViewModels

--- src/main/java/com/sg/viewmodel/people/personprofile/OrgViewModel.java
+++ src/main/java/com/sg/viewmodel/people/personprofile/OrgViewModel.java
@@ -1,4 +1,23 @@
 package com.sg.viewmodel.people.personprofile;
 
 public class OrgViewModel {
+
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
